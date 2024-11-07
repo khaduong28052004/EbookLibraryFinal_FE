78425c93d66676d8ab3afd35050f8dcc4b1800ab
phuckemz03 <146057737+phuckemz03@users.noreply.github.com>
2024-11-07 15:49:02 +0700

    UpdateAdmin-Seller

--- src/components/Breadcrumbs/Breadcrumb.tsx
+++ src/components/Breadcrumbs/Breadcrumb.tsx
@@ -1,8 +1,9 @@
 import { Link } from 'react-router-dom';
 interface BreadcrumbProps {
   pageName: string;
+  status: string;
 }
-const Breadcrumb = ({ pageName }: BreadcrumbProps) => {
+const Breadcrumb = ({ pageName, status }: BreadcrumbProps) => {
   return (
     <div className="mb-6 flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
       <h2 className="text-title-md2 font-semibold text-black dark:text-white">
@@ -13,7 +14,7 @@ const Breadcrumb = ({ pageName }: BreadcrumbProps) => {
         <ol className="flex items-center gap-2">
           <li>
             <Link className="font-medium" to="/">
-              Dashboard /
+              {status} /
             </Link>
           </li>
           <li className="font-medium text-primary">{pageName}</li>
