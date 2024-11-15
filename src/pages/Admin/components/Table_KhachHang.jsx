478c2c13a3360efa2e4905173a94d527512c6713
kimtuyen2511 <nguyentuyentctv2021@gmail.com>
2024-11-15 17:22:03 +0700

    updateFlashSale

--- src/pages/Admin/components/Table_KhachHang.jsx
+++ src/pages/Admin/components/Table_KhachHang.jsx
@@ -4,7 +4,7 @@ import { TrashIcon, ReceiptRefundIcon } from '@heroicons/react/24/outline'
 import Modal from "./ModalThongBao";
 import accountService from '../../../service/admin/Account';
 import { ExportExcel } from '../../../service/admin/ExportExcel';
-// import { usePDF } from 'react-to-pdf';
+import Pagination from './Pagination';
 
 const TableTwo = ({ onPageChange, onIdChange, entityData }) => {
     const [searchItem, setSearchItem] = useState('');
@@ -33,25 +33,6 @@ const TableTwo = ({ onPageChange, onIdChange, entityData }) => {
 
     const handleNext = () => {
         handlePageChange(entityData?.pageable?.pageNumber + 1);
-    };
-    const getPagesToShow = () => {
-        const totalPages = entityData?.totalPages || 0;
-        const current = entityData?.pageable?.pageNumber ?? 0;
-        const pages = [];
-        const maxPagesToShow = 5;
-
-        let start = Math.max(0, current - Math.floor(maxPagesToShow / 2));
-        let end = Math.min(totalPages - 1, start + maxPagesToShow - 1);
-
-        if (end - start + 1 < maxPagesToShow) {
-            start = Math.max(0, end - maxPagesToShow + 1);
-        }
-
-        for (let i = start; i <= end; i++) {
-            pages.push(i);
-        }
-
-        return pages;
     };
 
     useEffect(() => {
@@ -256,56 +237,14 @@ const TableTwo = ({ onPageChange, onIdChange, entityData }) => {
                     ))}
                 </tbody>
             </table>
-            <div className="py-6 flex border-t border-stroke  dark:border-strokedark  px-4 md:px-6 xl:px-7.5">
-                <div className="flex flex-1 justify-between sm:hidden">
-                    <a href="#" className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50">Previous</a>
-                    <a href="#" className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50">Next</a>
-                </div>
-                <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
-                    <div>
-                        <p className="text-sm text-gray-700 dark:text-white ">
-                            Showing
-                            <span className="font-medium"> {entityData?.pageable?.pageNumber * entityData.size + 1} </span>
-                            to
-                            <span className="font-medium"> {entityData.totalElements > (entityData.size * currentPage) ? ((entityData?.pageable?.pageNumber + 1) * entityData.size < entityData.totalElements ? (entityData?.pageable?.pageNumber + 1) * entityData.size : entityData.totalElements) : entityData.totalElements} </span>
-                            of
-                            <span className="font-medium"> {entityData.totalElements} </span>
-                            results
-                        </p>
-                    </div>
-                    <div>
-                        <nav className="isolate inline-flex -space-x-px rounded-md shadow-sm " aria-label="Pagination">
-                            <button
-                                onClick={handlePrevious} disabled={currentPage === 0}
-                                className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
-                                <span className="sr-only">Previous</span>
-                                <svg className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
-                                    <path fill-rule="evenodd" d="M11.78 5.22a.75.75 0 0 1 0 1.06L8.06 10l3.72 3.72a.75.75 0 1 1-1.06 1.06l-4.25-4.25a.75.75 0 0 1 0-1.06l4.25-4.25a.75.75 0 0 1 1.06 0Z" clip-rule="evenodd" />
-                                </svg>
-                            </button>
-                            {/* Pagination buttons */}
-                            {getPagesToShow().map((page) => (
-                                <button key={page} onClick={() => handlePageChange(page)}
-                                    className="relative dark:text-white hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex"
-                                >
-                                    {page + 1}
-                                </button>
-                            ))}
-
-
-                            <button
-                                onClick={handleNext}
-                                disabled={currentPage === entityData.totalPages - 1}
-                                className="relative dark:text-white inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
-                                <span className="sr-only">Next</span>
-                                <svg className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
-                                    <path fill-rule="evenodd" d="M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
-                                </svg>
-                            </button>
-                        </nav>
-                    </div>
-                </div>
-            </div>
+            <Pagination
+                pageNumber={currentPage}
+                totalPages={entityData?.totalPages}
+                totalElements={entityData?.totalElements}
+                handlePrevious={handlePrevious}
+                handleNext={handleNext}
+                setPageNumber={setCurrentPage}
+                size={entityData.size}></Pagination>
 
             <Modal
                 open={isOpen}
