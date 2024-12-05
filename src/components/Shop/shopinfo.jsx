000355efcb1644d7ce35d4f772d7cd338f0e374f
Thư Trần <tranngocanhthu23@gmail.com>
2024-12-06 01:52:08 +0700

    udate shopinfo address

--- src/components/Shop/shopinfo.jsx
+++ src/components/Shop/shopinfo.jsx
@@ -156,6 +156,19 @@ export default function ShopInfo({ shopData }) {
               )}</span>
             </div>
           </div>
+          <div className="flex py-2">
+            <div className="flex flex-1 gap-2 items-center items-center">
+              <div>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" class="size-5">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+                </svg>
+              </div>
+              <p>Địa chỉ:</p>
+              <span className="text-gray-600">{shopInfo.district}</span>
+            </div>
+
+          </div>
         </div>
       </div>
     </div>
