9faba8c3e54b17bbca31d87c55dd3619a89c25b9
Thư Trần <tranngocanhthu23@gmail.com>
2024-12-14 02:42:53 +0700

    udpate pagination home shopshop

--- src/components/Shop/index.jsx
+++ src/components/Shop/index.jsx
@@ -76,27 +76,13 @@ const vouchersdfEAsd = [
 
 const data_Products1 = {
     datas: [
-        {
-            id: 1,
-            name: 'Áo sơ mi nam cao cấp',
-            imageProducts: [
-                {
-                    name: 'https://down-bs-vn.img.susercontent.com/vn-11134216-7ras8-m1f4tydj1wt400_tn.webp',
-                },
-            ],
-            price: 500000,
-            sale: 20,
-            star: 4,
-            flashSaleDetail: {
-                sale: 15,
-            },
-        },
+
         {
             id: 2,
-            name: 'Giày thể thao nam',
+            name: 'Không gia đình',
             imageProducts: [
                 {
-                    name: 'https://down-bs-vn.img.susercontent.com/vn-11134216-7ras8-m1f4tydj1wt400_tn.webp',
+                    name: 'https://firebasestorage.googleapis.com/v0/b/ebookstore-4fbb3.appspot.com/o/evalue%2F330dc5d3-7e6d-40f9-8840-6d47b4fffdd5-khong_gia_dinh_8ae2708ff5.jpg?alt=media',
                 },
             ],
             price: 800000,
@@ -106,10 +92,10 @@ const data_Products1 = {
         },
         {
             id: 3,
-            name: 'Túi xách nữ thời trang',
+            name: 'Ra bờ suối ngắm hoa kèn hồng',
             imageProducts: [
                 {
-                    name: 'https://down-bs-vn.img.susercontent.com/vn-11134216-7ras8-m1f4tydj1wt400_tn.webp',
+                    name: 'https://firebasestorage.googleapis.com/v0/b/ebookstore-4fbb3.appspot.com/o/evalue%2Fa4395187-3b24-42c4-920e-dd577cc6101b-ra-bo-suoi-ngam-hoa-ken-hong.jpg?alt=media',
                 },
             ],
             price: 350000,
@@ -121,10 +107,10 @@ const data_Products1 = {
         },
         {
             id: 4,
-            name: 'Kính mát nam',
+            name: 'Hoàng tử  bé',
             imageProducts: [
                 {
-                    name: 'https://down-bs-vn.img.susercontent.com/vn-11134216-7ras8-m1f4tydj1wt400_tn.webp',
+                    name: 'https://firebasestorage.googleapis.com/v0/b/ebookstore-4fbb3.appspot.com/o/evalue%2F860b160b-317e-4c96-b70c-bd4591d651d8-HTB-review-sach.jpg?alt=media',
                 },
             ],
             price: 200000,
@@ -134,10 +120,10 @@ const data_Products1 = {
         },
         {
             id: 5,
-            name: 'Tai nghe Bluetooth',
+            name: 'Đắc nhân tâm',
             imageProducts: [
                 {
-                    name: 'https://down-bs-vn.img.susercontent.com/vn-11134216-7ras8-m1f4tydj1wt400_tn.webp',
+                    name: 'https://firebasestorage.googleapis.com/v0/b/ebookstore-4fbb3.appspot.com/o/avatar%2Fd4cdfa62-d6dc-4ba3-b430-f204182b8284-phan-4-dac-nhan-tam-1024x1024.jpg?alt=media',
                 },
             ],
             price: 150000,
@@ -147,49 +133,8 @@ const data_Products1 = {
                 sale: 30,
             },
         },
-        {
-            id: 6,
-            name: 'Đồng hồ thông minh',
-            imageProducts: [
-                {
-                    name: 'https://down-bs-vn.img.susercontent.com/vn-11134216-7ras8-m1f4tydj1wt400_tn.webp',
-                },
-            ],
-            price: 1000000,
-            sale: 15,
-            star: 4,
-            flashSaleDetail: null,
-        },
-        {
-            id: 7,
-            name: 'Chân váy xếp ly nữ',
-            imageProducts: [
-                {
-                    name: 'https://down-bs-vn.img.susercontent.com/vn-11134216-7ras8-m1f4tydj1wt400_tn.webp',
-                },
-            ],
-            price: 250000,
-            sale: 20,
-            star: 5,
-            flashSaleDetail: {
-                sale: 10,
-            },
-        },
-        {
-            id: 8,
-            name: 'Laptop gaming',
-            imageProducts: [
-                {
-                    name: 'https://down-bs-vn.img.susercontent.com/vn-11134216-7ras8-m1f4tydj1wt400_tn.webp',
-                },
-            ],
-            price: 15000000,
-            sale: 5,
-            star: 4,
-            flashSaleDetail: {
-                sale: 10,
-            },
-        },
+
+
     ],
 };
 
@@ -200,11 +145,15 @@ export default function ShopHome() {
     const [vouchers, setVouchers] = useState([]);  // Initialize as an empty array
     const [products, setProducts] = useState([]);  // Initialize as an empty array
     const [dataTopProducts, setDataTopProducts] = useState([]);  // Initialize as an empty array
-     const [loading, setLoading] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+    const [hasMore, setHasMore] = useState(true); // Có còn sản phẩm để tải không
+    const [page, setPage] = useState(1); // Trang hiện tại
+
     const [isFollowed, setIsFollowed] = useState(false);
     const local = useLocation();
     const query = new URLSearchParams(local.search);
     const { Id } = useParams();
+    const [size, setSize] = useState(8);
 
     const openModal = () => {
         setIsModalOpen(true);
@@ -224,14 +173,10 @@ export default function ShopHome() {
             console.log("fetch selectall error " + error);
         })
     }
-    useEffect(() => {
-        // fetchDataFlashSale();
-        fetchDataSelectAll();
-    }, [location]);
 
     const fetchShopInfo = async () => {
         try {
-            setLoading(true);
+            setIsLoading(true);
             const idAccountUser = sessionStorage.getItem("id_account");
             setIdUser(idAccountUser)
             const response = await homeShopService.fetchShopInfo(Id, idAccountUser);
@@ -239,7 +184,7 @@ export default function ShopHome() {
                 const data = response.data.result;
                 data != null ? setShopInfo(data) : setShopInfo(null);
                 setIsFollowed(response.data.result.shopDataEX.isFollowed);
-                setLoading(true);
+                setIsLoading(true);
             } else {
                 setShopInfo(data);
                 ;
@@ -248,19 +193,19 @@ export default function ShopHome() {
         } catch (error) {
             ;
         } finally {
-            setLoading(false);
+            setIsLoading(false);
         }
     };
 
     const fetchVoucherShopHome = async () => {
         try {
-            setLoading(true);
+            setIsLoading(true);
             const response = await homeShopService.fetchVoucherShopHome(Id);
             console.log(response);
             if (response.data.result) {
                 const data = response.data.result.Voucher;
                 setVouchers(data);
-                setLoading(true);
+                setIsLoading(true);
             } else {
                 throw new Error("Không có dữ liệu voucher từ API");
             }
@@ -268,50 +213,54 @@ export default function ShopHome() {
             setVouchers(vouchersdfEAsd);
             ;
         } finally {
-            setLoading(false);
+            setIsLoading(false);
         }
     }
 
     const fetchTopProducts = async () => {
         // setVouchers(vouchersdfEAsd);
         try {
-            setLoading(true);
+            setIsLoading(true);
             const response = await homeShopService.fetchTopProducts(Id);
-            console.log("response");
             if (response.data.result) {
                 const data = response.data.result;
                 setProducts(data);
-                setLoading(true);
+                setIsLoading(true);
             } else {
                 throw new Error('Không có dữ liệu');
             }
         } catch (error) {
             setProducts(data_Products1);
         } finally {
-            setLoading(false);
+            setIsLoading(false);
         }
     }
 
-    const fetchProductShopHome = async () => {
+    const fetchProductShopHome = async (currentSize) => {
         // setVouchers(vouchersdfEAsd);
         try {
-            setLoading(true);
-            const response = await homeShopService.fetchProductShopHome(Id);
-            console.log("response");
+            // setIsLoading(true);
+            const response = await homeShopService.fetchProductShopHome(Id, currentSize);
             if (response.data.result) {
-                const data = response.data.result;
-                setDataTopProducts(data);
-                setLoading(true);
+                const newProducts = response.data.result;
+                console.log("newProducts", newProducts);
+
+                setProducts(newProducts);
+                setHasMore(newProducts.datas.length >= currentSize)
             } else {
                 throw new Error('Không có dữ liệu');
             }
         } catch (error) {
             setProducts(data_Products1);
         } finally {
-            setLoading(false);
+            setIsLoading(false);
         }
     }
 
+    const handleLoadMore = () => {
+        if (hasMore && !isLoading)
+            setSize((prevSize) => prevSize + 8);
+    };
 
     useEffect(() => {
         fetchShopInfo();
@@ -319,6 +268,11 @@ export default function ShopHome() {
         fetchProductShopHome();
         fetchTopProducts();
     }, [])
+
+
+    useEffect(() => {
+        fetchProductShopHome(size);
+    }, [size])
 
     useEffect(() => {
         console.log("vouchers", vouchers);
@@ -329,11 +283,18 @@ export default function ShopHome() {
     }, [shopInfo])
 
     useEffect(() => {
-        console.log("shopInfo", shopInfo);
-    }, [products])
+        // Cập nhật visibleData mỗi khi visibleProducts thay đổi
+        console.log("products SectionStyleThreeHomeShop", products);
+    }, [products]);
 
 
-    if (loading) {
+    useEffect(() => {
+        // fetchDataFlashSale();
+        fetchDataSelectAll();
+    }, [location]);
+
+
+    if (isLoading) {
         return (
             <div className="flex justify-center items-center h-screen ">
                 <BeatLoader color="#56A0D3" />
@@ -370,10 +331,30 @@ export default function ShopHome() {
                         <SectionStyleThreeHomeShop
                             className="new-products mb-[60px] "
                             seeMoreUrl="/all-products"
-                            products={products}
+                            products={products} isLoading={isLoading}
                         />
                     </div>
+                    <div className=''>
+                        {hasMore && (
+                            <div className="flex justify-center mb-10">
+                                <button
+                                    onClick={handleLoadMore}
+                                    disabled={isLoading}
+                                    className="load-more border rounded border-[#003EA1] text-[#003EA1] px-20 py-1 bg-white hover:bg-[#003EA1] hover:text-white">
+                                    {isLoading ? "Đang tải..." : "Xem thêm"}
+                                </button>
+                            </div>
+                        )}
+                        {/* <div className="flex justify-center mb-10">
+                            <button
+                                onClick={handleLoadMore}
+                                className="load-more border rounded border-[#003EA1] text-[#003EA1] px-20 py-1 bg-white hover:bg-[#003EA1] hover:text-white">
+                                Xem thêm
+                            </button>
+                        </div> */}
+                    </div>
                 </div>
+
             </div>
         </Layout>
 
